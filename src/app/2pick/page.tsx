06b17593deed4d0a61adfa8e2b07cb432acba455
ようかん <pnasimonan@gmail.com>
2025-04-24 01:16:54 +0900

    [fix] 2pickの修正

--- src/app/2pick/page.tsx
+++ src/app/2pick/page.tsx
@@ -7,7 +7,8 @@ import yojoData from '@/data/yojo.json';
 import Link from 'next/link';
 import { DarkModeContext } from "../DarkModeProvider";
 import Deck from '@/components/Deck';
-import CardSelection from './components/CardSelection'; // 新しいコンポーネントをインポート
+import CardSelection from './components/CardSelection';
+import ExportPopup from '@/components/ExportPopup';
 
 // サンプルカードデータ
 const sampleCards: CardInfo[] = [
@@ -32,12 +33,16 @@ export default function TwoPick() {
   const [currentPhase, setCurrentPhase] = useState<CardType>('幼女');
   // 現在表示されている選択肢
   const [currentChoices, setCurrentChoices] = useState<CardInfo[]>([]);
+  // 幼女デッキ
+  const [yojoDeck, setYojoDeck] = useState<CardInfo[]>([]);
+  // お菓子デッキ
+  const [sweetDeck, setSweetDeck] = useState<CardInfo[]>([]);
 
   const { isDarkMode, toggleDarkMode } = useContext(DarkModeContext);
   
-  const [deck, setDeck] = useState<CardInfo[]>([]); // 幼女デッキ
   const [isShowDeck, setIsShowDeck] = useState(false); // デッキ確認ポップアップの表示状態
   const [round, setRound] = useState(1); // 現在のラウンド
+  const [showExportPopup, setShowExportPopup] = useState(false);
 
   // 選択肢を更新する関数
   const updateChoices = useCallback(() => {
@@ -62,24 +67,21 @@ export default function TwoPick() {
 
   // カードが選択されたときの処理
   const handleCardSelect = (card1: CardInfo, card2: CardInfo) => {
-    if (deck.length < 30) {
-      if (currentPhase === '幼女') {
-        setSelectedCards([...selectedCards, card1, card2]);
-        setDeck([...deck, card1, card2]);
-      } else {
-        setSelectedCards([...selectedCards, card1]);
-        setDeck([...deck, card1]);
-      }
-      // 20枚選択したらお菓子を選択
-      if (currentPhase === '幼女' && deck.length >= 20) {
-        setCurrentPhase('お菓子');
-        setRound(1);
-      }
+    if (currentPhase === '幼女' && yojoDeck.length < 20) {
+      setSelectedCards([...selectedCards, card1, card2]);
+      setYojoDeck([...yojoDeck, card1, card2]);
+    } else if (currentPhase === 'お菓子' && sweetDeck.length < 10) {
+      setSelectedCards([...selectedCards, card1, card2]);
+      setSweetDeck([...sweetDeck, card1, card2]);
+    }
 
-      setRound(round + 1);
+    setRound(round + 1);
+    updateChoices();
 
-      updateChoices();
-      
+    // 20枚選択したらお菓子を選択
+    if (currentPhase === '幼女' && round >= 10) {
+      setCurrentPhase('お菓子');
+      setRound(1);
     }
   };
 
@@ -87,8 +89,6 @@ export default function TwoPick() {
   const showDeck = () => {
     setIsShowDeck(isShowDeck => !isShowDeck);
   };
-
-
 
   return (
     <div className="container">
@@ -108,10 +108,10 @@ export default function TwoPick() {
       </header>
 
       <div className="mt-4 shadow-md flex flex-col items-center">
-        {deck.length < 30 ? (
+        {yojoDeck.length < 20 || sweetDeck.length < 10 ? (
           <>
             <h2 className="text-xl font-bold mb-4 text-center">
-              {round} / {currentPhase == "幼女" ? 10 : 5}: {currentPhase}カードを選択してください
+              {round} / {currentPhase === "幼女" ? 10 : 5}: {currentPhase}カードを選択してください
             </h2>
             <div className="flex justify-between w-full max-w-4xl items-center">
               {/* 左側のカード選択 */}
@@ -147,20 +147,49 @@ export default function TwoPick() {
           <div className="text-center">
             <h2 className="text-2xl font-bold mb-4">デッキ構築完了！</h2>
             <p className="mb-4">選択したカードでデッキが完成しました。</p>
-            <Link href="/" className="btn-export">
+            <Link href="/" className="btn-export mb-4">
               ホームに戻る
             </Link>
+            <button
+              className="btn-export mb-4"
+              onClick={() => setShowExportPopup(true)}
+            >
+              エクスポート
+            </button>
+            <div className="flex justify-center">
+                <button
+                  className="btn-import"
+                  onClick={() => showDeck()}
+                >
+                  デッキ確認
+                </button>
+              </div>
           </div>
-
         )}
         {isShowDeck && (
+          <div className="space-y-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 gap-2 mt-2">
             <Deck
-              cards={deck}
-              type={currentPhase}
+              cards={yojoDeck}
+              type="幼女"
               removeable={false}
-              ></Deck>
-          )}
+            />
+            <Deck
+              cards={sweetDeck}
+              type="お菓子"
+              removeable={false}
+            />
+          </div>
+        )}
       </div>
+
+      {/* エクスポートポップアップ */}
+      {showExportPopup && (
+        <ExportPopup
+          yojoDeck={yojoDeck}
+          sweetDeck={sweetDeck}
+          onClose={() => setShowExportPopup(false)}
+        />
+      )}
     </div>
   );
 }
