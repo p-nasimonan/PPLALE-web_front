62837a911d785e1b24e4ec0a1824103514d5f227
ようかん <pnasimonan@gmail.com>
2025-05-12 21:24:16 +0900

    [try]Githubpages環境をテストしたい

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,15 @@
 import type { NextConfig } from 'next';
 
 const isGitHubPages = process.env.GITHUB_PAGES === 'true';
+const basePath = isGitHubPages ? '/PPLALE-web_front' : '';
 
 const config: NextConfig = {
   output: 'export',
-  assetPrefix: isGitHubPages ? '/PPLALE-web_front' : '',
+  assetPrefix: basePath,
   images: {
     unoptimized: true,
     domains: ['localhost', 'pplale.pgw.jp'],
-    path: isGitHubPages ? '/PPLALE-web_front' : '',
+    path: basePath,
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     formats: ['image/webp'],
@@ -25,8 +26,11 @@ const config: NextConfig = {
       },
     ];
   },
-  basePath: isGitHubPages ? '/PPLALE-web_front' : '',
+  basePath,
   trailingSlash: false,
+  publicRuntimeConfig: {
+    basePath,
+  },
 };
 
 export default config;
